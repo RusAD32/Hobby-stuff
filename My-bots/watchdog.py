d769df01e49dead9c1203c6eeb674892fb13be83
RusAD32 <vseslav1997@gmail.com>
2017-09-19 16:06:48 +0300

    No more empty logs

--- My-bots/watchdog.py
+++ My-bots/watchdog.py
@@ -11,8 +11,10 @@ def bot_ls(bot):
         compproc = subprocess.run([f"./bots/{bot}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(f"A problem occured with {bot[:-3]}. See logs")
         with open(f"{bot[:-3]}_errlog", mode = 'a') as log:
-            log.write(compproc.stderr.decode("utf-8"))
-        dummy = subprocess.run(["./bots/dummy.py", f"{bot}"])
+            s = compproc.stderr.decode("utf-8")
+            if len(s) > 5:
+                log.write(s)
+                dummy = subprocess.run(["./bots/dummy.py", f"{bot}"])
         time.sleep(10)
 
 if __name__ == "__main__":
