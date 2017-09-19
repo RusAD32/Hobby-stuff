e4d471a30efb693f8328b19a8b21a90c8d94f063
unknown <justbucket416@gmail.com>
2017-09-19 16:02:37 +0300

    make it better

--- My-bots/bots/just_bot.py
+++ My-bots/bots/just_bot.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
-
 import calendar
 import datetime
 import os
 import pickle
 import time
+
 import pyowm
 import schedule
 import telebot
-import sys
 from requests import exceptions
 from tinydb import TinyDB, Query
 
@@ -16,21 +14,11 @@ weekdays = {'понедельник': 'Monday', 'вторник': 'Tuesday', 'с
             'пятницу': 'Friday', 'субботу': 'Saturday'}
 
 now = datetime.datetime.now()
-if os.path.exists("just_bot_owm_token"):
-    with open("just_bot_owm_token") as ot:
-        owm = pyowm.OWM(ot.read().strip())
-else:
-    sys.stderr.write("No owm token found, exiting...\n")
-    sys.exit(1)
-if os.path.exists("just_bot_token"):
-    with open("just_bot_token") as t:
-        justbot = telebot.TeleBot(t.read().strip())
-else:
-    sys.stderr.write("No Telegram token found, exiting...")
-    sys.exit(2)
+owm = pyowm.OWM('e8eb26da01b8f30c029efefbf897846d')
+justbot = telebot.TeleBot('340325873:AAGSVf33Gz59sbcqlj8BIkBgmTg3424V8yE')
 
 que = Query()
-db = TinyDB('./bots/data.db')
+db = TinyDB('data.db')
 
 def get_sched(day, table):
     sched = ''
@@ -38,12 +26,12 @@ def get_sched(day, table):
     for item in table.search((que.weekday == day) & ((que.week=="Both") | (que.week == (int(now.strftime("%W")))% 2))):
         sched += str(item['num']) + '. ' + item['time'] + ' ' + item['name'] + \
                ', ' + str(item['hall']) + ', ' + item['lecturer'] + '\n'
-    if (sched==''): st="В день нет пар"
+    if (sched==''): st = None
     else: st += sched
     return st
 
 def save():
-    with open('./bots/justpickle', mode='wb') as out:
+    with open('justpickle', mode='wb') as out:
         pickle.dump([users], out, protocol=pickle.HIGHEST_PROTOCOL)
 
 @justbot.message_handler(commands=['start'])
@@ -55,7 +43,7 @@ def send_welcome(message):
 
 @justbot.message_handler(commands=['set_group'])
 def set_default_group(message):
-    if message.text.split(' ')[1] in db.tables():
+    if len(message.text.split(' ')) == 2 and message.text.split(' ')[1] in db.tables():
         users.update({message.from_user.id:message.text.split(' ')[1]})
         save()
         justbot.send_message(message.chat.id, 'Changed successfully')
@@ -74,13 +62,22 @@ def show_schedule(message):
     if 'расписание' in message.text.lower():
         for word in message.text.split(' '):
             if word in weekdays.keys():
-                justbot.send_message(message.chat.id, get_sched(weekdays.get(word), db.table(users[message.from_user.id])))
+                text = get_sched(weekdays.get(word), db.table(users[message.from_user.id]))
+                if text == None: text = 'В ' + word + ' нет пар'
+                justbot.send_message(message.chat.id, text)
                 break
             elif word == "завтра":
-                justbot.send_message(message.chat.id, get_sched(calendar.day_name[int(now.strftime('%w'))], db.table(users[message.from_user.id])))
+                text = get_sched(calendar.day_name[int(now.strftime('%w'))], db.table(users[message.from_user.id]))
+                if text == None: text = 'Йеееей! Завтра нет пар!'
+                justbot.send_message(message.chat.id, text)
                 break
             elif word == "сегодня":
-                justbot.send_message(message.chat.id, get_sched(now.strftime("%A"), db.table(users[message.from_user.id])))
+                text = get_sched(now.strftime("%A"), db.table(users[message.from_user.id]))
+                if text == None: text = 'Сегодня нет пар!'
+                justbot.send_message(message.chat.id, text)
+                break
+    elif 'расписание на расписание' in message.text.lower():
+        justbot.send_message(message.chat.id, 'Совсем с цма сошёл?')
     elif 'погода' in message.text.lower():
         forecast = owm.weather_at_place('Moscow,ru').get_weather().get_temperature(unit='celsius')
         weather = 'Температура воздуха - ' + str(forecast)[8:13] + ' градусов Цельсия\n'
@@ -91,13 +88,12 @@ def scheduler():
         schedule.run_pending()
         time.sleep(1)
 
-def morning():
-    schedule.every().day.at('07:00').do(justbot.send_message("208109844", 'Приветствую! Сегодня ' + now.strftime('%x') + '\n' + 'Температура воздуха - ' + str(owm.weather_at_place('Moscow,ru').get_weather().get_temperature(unit='celsius'))[8:14] + ' градусов Цельсия\nРасписание на сегодня:\n' + get_sched(now.strftime("%A"), db.table('trash)'))))
-
-if os.path.exists("./bots/justpickle"):
-    with open("./bots/justpickle", mode = "rb") as data:
+if os.path.exists("./justpickle"):
+    with open("./justpickle", mode = "rb") as data:
         users = pickle.load(data)[0]
 else: users = {}
+
+#schedule.every().day.at('07:00').do(justbot.send_message("208109844", 'Приветствую! Сегодня ' + now.strftime('%x') + '\n' + 'Температура воздуха - ' + str(owm.weather_at_place('Moscow,ru').get_weather().get_temperature(unit='celsius'))[8:14] + ' градусов Цельсия\nРасписание на сегодня:\n' + get_sched(now.strftime("%A"), db.table('trash')), db.table('trash)')))
 
 while True:
     try:
